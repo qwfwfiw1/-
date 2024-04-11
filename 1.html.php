6ff2f0a32cda5d7160e18c6e294cbc2c3e84095d
qwfwfiw1 <144986344+qwfwfiw1@users.noreply.github.com>
2024-04-12 02:34:55 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,10 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; // 在網頁中輸出 "Hello world!" ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; // 再次在網頁中輸出 "Hello world!" ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; // 一次再次在網頁中輸出 "Hello world!" ?> 
+    </body>
+</html>
